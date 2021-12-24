83b0b01a391f2df4fe65cb2b20e5839a25966427
Luan Santos <luanlss10@outlook.com>
2021-12-24 04:29:10 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+export default function appCamera() {
+ return (
+   <View>
+     <StatusBar hidden={true}/>
+     <Text> App Camera</Text>
+   </View>
+  );
+}
